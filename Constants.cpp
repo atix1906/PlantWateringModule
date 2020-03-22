f37ea72ea900a4e2bec3cee042c3966db54e0f4d
atix1906 <27219487+atix1906@users.noreply.github.com>
2020-03-22 15:29:04 +0100

    Update Constants.cpp

--- Constants.cpp
+++ Constants.cpp
@@ -1,5 +1,6 @@
 #include "Constants.h"
 
+// Fill in the correct information for your mqtt broker and wifi
 const char* GlobalConstants::ssid = "ssid";
 const char* GlobalConstants::password = "passwd";
 const char* GlobalConstants::ipAddress = "ip-broker";
