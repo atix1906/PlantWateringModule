4c98ac19418bff40d7a757cd5c5ebce39de737f4
atix1906 <27219487+atix1906@users.noreply.github.com>
2020-03-24 10:38:54 +0100

    update sketch and circuit layout

--- PlantWateringModule.ino
+++ PlantWateringModule.ino
@@ -1,15 +1,25 @@
 /*
-  SimpleMQTTClient.ino
-  The purpose of this exemple is to illustrate a simple handling of MQTT and Wifi connection.
-  Once it connects successfully to a Wifi network and a MQTT broker, it subscribe to a topic and send a message to it.
-  It will also send a message delayed 5 seconds later.
+  PlantWateringModule.ino
+  This sketch uses a ESP8266 Module and has a moisture sensor and a waterpump connected to it.
+  So whenever the moisture sensor registers a lack of water, the pump will water it again.
 */
 
 #include "EspMQTTClient.h"
 #include "Constants.h"
 
-int counter = 0;
-unsigned int counter2 = 0;
+int cnt = 0;
+
+String moduleName = "PlantWatering"; //ToDo: replace text with this variable
+float moistureValue = 0;
+double moisPercent = 0;
+int maxValue = 600;
+
+const int waterPump = 5;
+const int sensorInput = 0;
+const int sensorOnOff = 4;
+
+bool waterGo = false;
+bool debug = true;
 
 EspMQTTClient client(
   GlobalConstants::ssid,
@@ -17,98 +27,150 @@ EspMQTTClient client(
   GlobalConstants::ipAddress,  // MQTT Broker server ip
   "MQTTUsername",   // Can be omitted if not needed
   "MQTTPassword",   // Can be omitted if not needed
-  "PostBox",     // Client name that uniquely identify your device
+  "PlantWatering",     // Client name that uniquely identify your device
   1883              // The MQTT port, default to 1883. this line can be omitted
 );
 
 void getStatus(const String & payload) {
   if (payload == "getStatus") {
-    client.publish("Briefkasten/response", "Briefkasten is alive");
-    
-    //client.publish("Briefkasten/response", (unsigned char)sensorValue);
+    client.publish("PlantWatering/response", "PlantWatering is alive");
   }
 }
 
 void setup()
 {
   Serial.begin(115200);
-
- // noInterrupts();
- // timer0_isr_init();
- // timer0_attachInterrupt(servoISR)
-  // pinMode(D6  , OUTPUT);
-  setOutputPins();
-  //pinMode(interruptPin, INPUT_PULLUP);
-  //attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, RISING);
-  // Optionnal functionnalities of EspMQTTClient :
-  //client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
-  //client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overrited with enableHTTPWebUpdater("user", "password").
-  client.enableLastWillMessage("TestClient/lastwill", "I am going offline");  // You can activate the retain flag by setting the third parameter to true
+  pinMode(waterPump,OUTPUT);
+  pinMode(sensorInput,INPUT);
+  pinMode(sensorOnOff,OUTPUT);
+  if(debug){
+    client.enableDebuggingMessages(); //Enable debugging messages sent to serial output
+  }
+  client.enableLastWillMessage("PlantWatering/status", "I am going offline",true);  // You can activate the retain flag by setting the third parameter to true
 }
 
 // This function is called once everything is connected (Wifi and MQTT)
 // WARNING : YOU MUST IMPLEMENT IT IF YOU USE EspMQTTClient
 void onConnectionEstablished()
 {
-  // Subscribe to "Briefkasten/test" and display received message to Serial
-  client.subscribe("Briefkasten/test", [](const String & payload) {
+  // Subscribe to "PlantWatering/test" and display received message to Serial
+  client.subscribe("PlantWatering/test", [](const String & payload) {
     Serial.println(payload);
   });
 
-  client.subscribe("Briefkasten/request", [](const String & payload) {
+  client.subscribe("PlantWatering/request", [](const String & payload) {
     getStatus(payload);
     Serial.println(payload);
   });
      
   // Publish a message to "Briefkasten/test"
-  client.publish("Briefkasten/inbox", "Briefkasten is connected"); // You can activate the retain flag by setting the third parameter to true
+  client.publish("PlantWatering/status", "PlantWatering is connected",true); // You can activate the retain flag by setting the third parameter to true
   
   // Execute delayed instructions
-  client.executeDelayed(5 * 1000, []() {
-    client.publish("Briefkasten/inbox", "Check your letterbox!");
+  /*client.executeDelayed(5 * 1000, []() {
+    client.publish("PlantWatering/status", "Not sure what to say yet!");
     counter = 0;
-  });
+  });*/
 
-   //client.publish("Briefkasten/test", "loop",true); // You can activate the retain flag by setting the third parameter to true
+   //client.publish("PlantWatering/test", "loop",true); // You can activate the retain flag by setting the third parameter to true
 }
 
 void goingToSleep(){
   yield();
   //setOutputPins();
-  setPinsLow();
+  
   Serial.println("going to deepsleep");
   delay(100);
-  ESP.deepSleep(0);
+  ESP.deepSleep(20e6);
   yield();
 }
 
-void loop() {
-  /*float moistureValue = analogRead(analogInput);
-  if (moistureValue > maxValue){
-    maxValue = moistureValue;
+void startTheWaterFlow(){
+  digitalWrite(waterPump,HIGH);
+  if(debug){
+    Serial.println("Water is flowing!!!");
   }
-  Serial.print("\nmoisture Value: ");
-  Serial.print(moistureValue);
-  Serial.print("\tmax Value: ");
-  Serial.print(maxValue);
-  
-  double moisPercent = (moistureValue/maxValue)*100.0;
-  Serial.print("\tProzent: ");
-  Serial.print(moisPercent);
-  if(moisPercent < 20){
-    digitalWrite(ledGreen,LOW);
-    digitalWrite(ledYellow,LOW);
-    digitalWrite(ledRed,HIGH);
-  } 
-  else if (moisPercent >=20 && moisPercent <40){
-    digitalWrite(ledGreen,LOW);
-    digitalWrite(ledYellow,HIGH);
-    digitalWrite(ledRed,LOW);
+}
+
+void stopTheWaterFlow(){
+  digitalWrite(waterPump,LOW);
+  if(debug){
+    Serial.println("Pump is shut off");
+  }
+}
+
+void moistureSensorOn(){
+  digitalWrite(sensorOnOff,HIGH);
+}
+
+void moistureSensorOff(){
+  digitalWrite(sensorOnOff,LOW);
+}
+
+void sendData(double data){
+  String output = "";
+  output = String(data,2);
+  client.publish("PlantWatering/data", output); // You can activate the retain flag by setting the third parameter to true
+}
+
+
+void loop() {
+  client.loop();
+  if(debug){
+    Serial.println(cnt);
+  }
+  if(cnt < 30){
+    moistureSensorOn();
+    moistureValue = analogRead(sensorInput);
+    if (moistureValue > maxValue){
+      maxValue = moistureValue;
+    }
+    if(debug){
+      Serial.print("\nmoisture Value: ");
+      Serial.print(moistureValue);
+      Serial.print("\tmax Value: ");
+      Serial.println(maxValue);
+    }
   }
   else{
-    digitalWrite(ledGreen,HIGH);
-    digitalWrite(ledYellow,LOW);
-    digitalWrite(ledRed,LOW);
+    moistureSensorOff();
+    moisPercent = (moistureValue/maxValue)*100.0;
+    if(debug){
+      Serial.print("\tProzent: ");
+      Serial.print(moisPercent);
+      Serial.println();
+    }
+    sendData(moisPercent);
+    if(moisPercent < 20){
+      if(debug){
+        Serial.println("Water...I need water....");
+      }
+      waterGo = true;
+    } 
+    else if (moisPercent >=20 && moisPercent <40){
+      if(debug){
+        Serial.println("I will be thirsty soon.");
+      }
+    }
+    else{
+      if(debug){
+        Serial.println("I'm good, thanks for asking!");
+      }
+    }
   }
-  delay(1000);*/
+
+  if(waterGo){
+    if(cnt < 80){
+      startTheWaterFlow();
+    }
+    else{
+      stopTheWaterFlow();
+    }
+  }
+
+  if(cnt > 110){
+    goingToSleep();
+  }
+  cnt++;
+  delay(100);
 }
